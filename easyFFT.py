ac15881fd3e65888d921d8a7f4173b7c1f79829e
froohoo <froohoo@gmail.com>
2018-10-13 20:27:03 -0500

    Clean up commented code

--- easyFFT.py
+++ easyFFT.py
@@ -27,18 +27,21 @@ inp.setrate(capset['RATE'])
 inp.setformat(capset['FORMAT'])
 inp.setperiodsize(capset['PERIODSZ'])
 
+# Set up plots
 freq_plt = plt.subplot(212)
 time_plt = plt.subplot(211)
 plt.ion()
 plt.show()
 
-
+# Calculate the number of datapoints we will have in a sample
 x = np.arange( ( (capset['RATE'] * capset['SAMPLESZ']) //
                  (capset['PERIODSZ']) + 
                  ( (capset['RATE'] * capset['SAMPLESZ']) %
                   capset['PERIODSZ'] > 0) ) * capset['PERIODSZ'] )
+
 print("<----- PRESS ENTER KEY IN THIS TERMINAL TO TERMINATE PROCSSING ----->")
 print("Lossless capture rate will be %d frames"%(len(x),))
+
 init_ary = np.zeros(len(x))
 time_line, = time_plt.plot(x, init_ary, 'b')
 freq_line, = freq_plt.plot(x, init_ary, 'r')
@@ -47,9 +50,7 @@ freq_plt.set_xlim(0, 20000)
 time_plt.set_ylim(-1,1)
 time_plt.set_xlim(0,200)
 
-#time_plt, freq_plt = plt.plot(x,init_ary,'b', x, init_ary,'r' )
-
-# Spin off thread to watch for 'Enter' press
+# Thread to watch for 'Enter' press
 #
 def kb_thread(run):
     input()
@@ -59,14 +60,13 @@ run = [True]
 audio_in = [] 
 thread.start_new_thread(kb_thread, (run,))
 
-# Spin off thread to collect Audio data
+# Thread to collect Audio data
 # 
 def rec_thread(run, inp, audio_in, settings, xlen):
     local_buf = []
     while run:
         while settings['SAMPLESZ'] > len(local_buf)/settings['RATE']:
             l, data = inp.read()
-            #assert(l>0)
             if l:
                 local_buf.extend([unpack('h', data[i:i+2])[0] for i in range(0,2*l,2)])
         audio_in.extend(local_buf)    
@@ -79,7 +79,6 @@ thread.start_new_thread(rec_thread, (run, inp, audio_in, capset, len(x)))
 
 while run:
     if audio_in:
-        #print("audio_in length: ", len(audio_in))
         normal_audio = np.array(audio_in)/2.**15
         n = len(normal_audio)
         Nn = np.fft.fft(normal_audio)
@@ -90,7 +89,6 @@ while run:
         freq_line.set_ydata(psd)
         time_line.set_xdata(np.arange(0, 1000, 1000/n ))
         time_line.set_ydata(normal_audio)
-        #plt.plot(x, normal_audio)
         plt.pause(0.001)
         audio_in.clear()
 
